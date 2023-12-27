6fa783b1926f96164aa46a03a44cb3365ca0a49c
Nathaniel Wilson <88940348+Nathanos56@users.noreply.github.com>
2023-12-27 15:53:46 -0700

    Add files via upload

--- update_git.py
+++ update_git.py
@@ -1,34 +1,39 @@
 import git
 import os
 import subprocess
+import time
 
 app_location = "Window_Display"
 
-#update dependancies
-subprocess.run(["python3", "dependancies.py"])
+while True:
+    #update dependancies
+    subprocess.run(["python3", "dependancies.py"])
 
-# Get the home directory in a cross-platform way
-home_dir = os.path.expanduser("~")
+    # Get the home directory in a cross-platform way
+    home_dir = os.path.expanduser("~")
 
-# Construct the path to the directory
-dir_path = os.path.join(home_dir, app_location)
+    # Construct the path to the directory
+    dir_path = os.path.join(home_dir, app_location)
 
-# Navigate to the directory
-os.chdir(dir_path)
+    # Navigate to the directory
+    os.chdir(dir_path)
 
-# Initialize a Git object
-repo = git.Repo('.')
+    # Initialize a Git object
+    repo = git.Repo('.')
 
-# Fetch the latest changes from the main branch
-repo.remotes.origin.fetch('main')
+    # Fetch the latest changes from the main branch
+    repo.remotes.origin.fetch('main')
 
-# Get the commit hash of the local and remote main branch
-local_commit = repo.heads.main.commit.hexsha
-remote_commit = repo.remotes.origin.refs.main.commit.hexsha
+    # Get the commit hash of the local and remote main branch
+    local_commit = repo.heads.main.commit.hexsha
+    remote_commit = repo.remotes.origin.refs.main.commit.hexsha
 
-# If the commit hashes are not the same, pull the latest changes
-if local_commit != remote_commit:
-    repo.remotes.origin.pull('main')
+    # If the commit hashes are not the same, pull the latest changes
+    if local_commit != remote_commit:
+        repo.remotes.origin.pull('main')
+    
+    #checks github every 5 minutes
+    time.sleep(300)
 
-print("Suck that")
-print("it worked")
+#print("Suck that")
+#print("it worked")
