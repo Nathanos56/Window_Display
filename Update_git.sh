beab9ad6e93c9d33c7f306e6894ed9abf07c5d39
Nathaniel Wilson <88940348+Nathanos56@users.noreply.github.com>
2023-12-27 12:23:49 -0700

    Delete Update_git.sh

--- Update_git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /Home/Window_Display/
-git fetch origin main
-LOCAL=$(git rev-parse main)
-REMOTE=$(git rev-parse origin/main)
-if [ $LOCAL != $REMOTE ]; then
-    git pull origin main
-fi
