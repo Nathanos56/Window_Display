b660adeda26c79343c80c1f865e1c9e649302883
Nathaniel Wilson <88940348+Nathanos56@users.noreply.github.com>
2023-12-27 14:57:16 -0700

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-from PIL_and_Tkinter import *
-import sys
-
-
-pilImage = Image.open(r"Portrait.jpg")
-showPIL(pilImage, "white")
-pilImage2 = Image.open(r"Landscape.jpg")
-showPIL(pilImage2, "black")
