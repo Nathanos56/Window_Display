8c2d7a3c2f2123d619064ca6c914f8ed0ca6840c
Nathaniel Wilson <88940348+Nathanos56@users.noreply.github.com>
2023-12-27 16:35:04 -0700

    Add files via upload

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ import os
 
 def run_script(script_path):
     # Create a detached process running the script
-    subprocess.Popen(["python", script_path], creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+    subprocess.Popen(["python", script_path])
 
 # Call the function with the path to your script
 run_script("update_git.py")
