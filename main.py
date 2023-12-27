61f330ff76e6d7c92ff395273fe383e5d6478d0b
Nathaniel Wilson <88940348+Nathanos56@users.noreply.github.com>
2023-12-27 15:38:08 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from PIL_and_Tkinter import *
 import sys
 import subprocess
 
+import os
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
 subprocess.run(["python3", "dependancies.py"])
 print(f"ran subprocess")
 
