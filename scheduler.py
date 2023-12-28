2914e6052568cd8647009f8af4220341491355ca
Nathaniel Wilson <88940348+Nathanos56@users.noreply.github.com>
2023-12-27 18:19:35 -0700

    Add files via upload

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,8 @@
 import subprocess
+# Get the home directory in a cross-platform way
+home_dir = os.path.expanduser("~")
 #update dependancies
-subprocess.call(["python3", "dependancies.py"])
+subprocess.call(["python3", f"{home_dir}/Window_Display/dependancies.py"])
 
 import os
 from daemonize import Daemonize
@@ -9,7 +11,7 @@ import schedule
 
 def run_script():
     # Create a detached process running the script
-    subprocess.Popen(["python3", "update_git.py"])
+    subprocess.Popen(["python3", f"{home_dir}/Window_Display/update_git.py"])
 
 # Create a daemon
 daemon = Daemonize(app="update_git", pid="/tmp/update_git.pid", action=run_script)
